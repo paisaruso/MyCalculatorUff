194f1c30dfa679227309b35ab2331182542f4f4c
paisaruso <enrique.oregoudea.edu.co>
2024-06-11 13:37:32 -0500

    Jodido

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,41 @@
-package org.example;public class Main {
+package org.example;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+public class Main {
+    static int option;
+    static String menu;
+
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OperacionesBasicas object = new OperacionesBasicas();
+
+        do {
+            menu = JOptionPane.showInputDialog("MENU PRINCIPAL\n1. Suma\n2. Resta\n3. División\n4 Multiplicación\n5. Salir");
+            option = Integer.parseInt(menu);
+            switch (option) {
+                case 0:
+                    object.integrales();
+                case 1:
+                    object.suma();
+                    break;
+                case 2:
+                    object.resta();
+                    break;
+                case 3:
+                    object.division();
+                    break;
+                case 4:
+                    object.multiplicacion();
+                case 5:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog((Component)null, "La opción " + option + " es incorrecta");
+            }
+        } while(option != 5);
+
     }
 }
--- src/main/java/org/example/OperacionesBasicas.java
+++ src/main/java/org/example/OperacionesBasicas.java
@@ -1,2 +1,45 @@
-package org.example;public class OperacionesBasicas {
+package org.example;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+public class OperacionesBasicas {
+    double number1 = 0.0;
+    double number2 = 0.0;
+    double resultado = 0.0;
+
+    public OperacionesBasicas() {
+    }
+
+    public void suma() {
+        this.number1 = Double.parseDouble(JOptionPane.showInputDialog("Digite el primer valor"));
+        this.number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite el segundo valor"));
+        this.resultado = this.number1 + this.number2;
+        JOptionPane.showMessageDialog((Component)null, "El resultado es " + this.resultado);
+    }
+
+    public void resta() {
+        this.number1 = Double.parseDouble(JOptionPane.showInputDialog("Digite el primer valor"));
+        this.number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite el segundo valor"));
+        this.resultado = this.number1 - this.number2;
+        JOptionPane.showMessageDialog((Component)null, "El resultado es " + this.resultado);
+    }
+
+    public void division() {
+        this.number1 = Double.parseDouble(JOptionPane.showInputDialog("Digite el primer valor"));
+        this.number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite el segundo valor"));
+        this.resultado = this.number1 / this.number2;
+        JOptionPane.showMessageDialog((Component)null, "El resultado es " + this.resultado);
+    }
+
+    public void multiplicacion() {
+        this.number1 = Double.parseDouble(JOptionPane.showInputDialog("Digite el primer valor"));
+        this.number2 = Double.parseDouble(JOptionPane.showInputDialog("Digite el segundo valor"));
+        this.resultado = this.number1 * this.number2;
+        JOptionPane.showMessageDialog((Component)null, "El resultado es " + this.resultado);
+    }
+
+    public void integrales() {
+    }
 }
+
